12baf5ab3b80987a79c3d11c2d7974ca752904a3
danielfercope <danielfercope@gmail.com>
2024-12-01 22:58:14 -0300

    Cadastros de produtos com sales_price

--- lib/screens/production/register/product_register.dart
+++ lib/screens/production/register/product_register.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 
 class ProductRegistrationScreen extends StatefulWidget {
   @override
-  _ProductRegistrationScreenState createState() => _ProductRegistrationScreenState();
+  _ProductRegistrationScreenState createState() =>
+      _ProductRegistrationScreenState();
 }
 
 class _ProductRegistrationScreenState extends State<ProductRegistrationScreen> {
@@ -11,6 +13,11 @@ class _ProductRegistrationScreenState extends State<ProductRegistrationScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _weightController = TextEditingController();
   final TextEditingController _categoryController = TextEditingController();
+  final MoneyMaskedTextController _priceController = MoneyMaskedTextController(
+    leftSymbol: 'R\$',
+    decimalSeparator: ',',
+    thousandSeparator: '.',
+  );
 
   String? _selectedReceipeId;
   List<dynamic> _receipes = [];
@@ -28,33 +35,57 @@ class _ProductRegistrationScreenState extends State<ProductRegistrationScreen> {
         _receipes = response.data['data'];
       });
     } catch (e) {
-      print('Error fetching receipes: $e');
+      print('Erro ao buscar receitas: $e');
     }
   }
 
   Future<void> _submitProduct() async {
     if (_formKey.currentState!.validate() && _selectedReceipeId != null) {
+      // Construindo o JSON
       final productData = {
-        "name": _nameController.text,
-        "wheight": double.tryParse(_weightController.text) ?? 0.0,
         "category": _categoryController.text,
+        "name": _nameController.text,
         "receipe": _selectedReceipeId,
+        "sale_price": _priceController.numberValue,
+        // Preço formatado
+        "wheight": double.tryParse(_weightController.text) ?? 0.0,
+        // Peso convertido
       };
 
       try {
+        // Enviando requisição
         await Dio().post(
           'http://localhost:8083/product',
+          options: Options(
+            headers: {
+              'Content-Type': 'application/json'
+            }, // Definindo cabeçalho
+          ),
           data: productData,
         );
+
+        // Notificação de sucesso
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Produto cadastrado com sucesso!')),
         );
+
+        // Limpando os campos após sucesso
+        _formKey.currentState!.reset();
+        _nameController.clear();
+        _weightController.clear();
+        _categoryController.clear();
+        _priceController.updateValue(0);
+        setState(() {
+          _selectedReceipeId = null;
+        });
       } catch (e) {
+        // Notificação de erro
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Erro ao cadastrar produto: $e')),
         );
       }
     } else {
+      // Campos obrigatórios não preenchidos
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Preencha todos os campos obrigatórios.')),
       );
@@ -77,7 +108,7 @@ class _ProductRegistrationScreenState extends State<ProductRegistrationScreen> {
                 controller: _nameController,
                 decoration: InputDecoration(labelText: 'Nome do Produto'),
                 validator: (value) => value == null || value.isEmpty
-                    ? 'O nome do produto é obrigatório'
+                    ? 'O nome é obrigatório'
                     : null,
               ),
               TextFormField(
@@ -94,6 +125,13 @@ class _ProductRegistrationScreenState extends State<ProductRegistrationScreen> {
                 validator: (value) => value == null || value.isEmpty
                     ? 'A categoria é obrigatória'
                     : null,
+              ),
+              TextFormField(
+                controller: _priceController,
+                decoration: InputDecoration(labelText: 'Preço'),
+                keyboardType: TextInputType.number,
+                validator: (value) =>
+                    _priceController.numberValue <= 0 ? 'Preço inválido' : null,
               ),
               DropdownButtonFormField<String>(
                 value: _selectedReceipeId,
